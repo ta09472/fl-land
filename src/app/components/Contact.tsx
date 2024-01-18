458c8baab9099eb38f5227f68bfcc4ccb172e77a
beomsuKim <ta09472@naver.com>
2024-01-18 12:14:40 +0900

    footer 수정

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -44,12 +44,24 @@ export default function Contact() {
     >
       <div className="flex flex-col justify-center items-center gap-4">
         <p className="block text-[2rem] font-extrabold ">찾아오시는 길</p>
-        <div className="flex flex-row items-center">
-          <div className="flex flex-col">
-            <p className="block">주소 : 전북 전주시 덕진구 전미중앙로 21</p>
-          </div>
 
-          <div id="map" className="w-[30rem] h-[30rem]" />
+        <div className="flex flex-row justify-between gap-4">
+          <div className="flex flex-col justify-center  text-left">
+            <p className="block font-semibold text-[1rem]">
+              이메일: ㅁㄴㅇ@ㅁㅇㄴ
+            </p>
+            <p className="block font-semibold text-[1rem]">
+              연락처: 010-231=123123
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div id="map" className="w-[30rem] h-[30rem]" />
+            <div className="flex flex-col">
+              <p className="block font-semibold text-[1rem]">
+                주소 : 전북 전주시 덕진구 전미중앙로 21
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,9 +25,14 @@ export default function Home() {
         <Contact />
       </div>
       <footer className=" flex flex-col px-64 py-4 bg-slate-50   text-slate-700">
-        <span>E-mail : festival-land@gmail.com</span>
-
-        <span>Copyright 2024. Festival Land. All Rights Reserved.</span>
+        <p className="font-semibold block">페스티벌 랜드</p>
+        <p className="block">사업자 등록번호 : 11111 | 대표 : 김현진</p>
+        <p className="block">
+          주식회사 페스티벌랜드 | 제조판매업 신고번호 : 2014-전주-03377
+        </p>
+        <p className="block">
+          우편번호(01237) | 전라북도 전주시 덕진구 전미중앙로 21
+        </p>
       </footer>
     </>
   );
