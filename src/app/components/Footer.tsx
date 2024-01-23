d8da86e842e33f0a7b24facf39b33e05fd545251
beomsuKim <ta09472@naver.com>
2024-01-23 21:19:10 +0900

    연혁 내용 추가

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,9 +3,7 @@ export default function Footer() {
     <footer className=" flex flex-col px-4 py-2 lg:px-64 lg:py-4 bg-slate-50   text-slate-700 text-sm lg:text-md dark:text-white dark:bg-darkCharcoal transition-colors duration-300">
       <p className="font-semibold block">페스티벌 랜드</p>
       <p className="block">사업자 등록번호 : 11111 | 대표 : 김현진</p>
-      <p className="block">
-        주식회사 페스티벌랜드 | 제조판매업 신고번호 : 2014-전주-03377
-      </p>
+      <p className="block">제조판매업 신고번호 : 2014-전주-03377</p>
       <p className="block">
         우편번호(01237) | 전라북도 전주시 덕진구 전미중앙로 21
       </p>
--- src/app/components/History.tsx
+++ src/app/components/History.tsx
@@ -1,10 +1,36 @@
+"use client";
+
+import { Steps, Tag, Timeline } from "antd";
+
 export default function History() {
   return (
     <section
       id="history"
-      className=" flex items-center justify-center min-h-screen text-center text-black relative bg-slate-100 py-52 max-md:pb-16 max-md:pt-48 overflow-hidden lqd-is-in-view dark:bg-darkCharcoal transition-colors duration-300"
+      className=" flex items-center justify-center min-h-screen text-center text-black relative bg-slate-100 py-52 max-md:pb-16 max-md:pt-48 overflow-hidden lqd-is-in-view dark:bg-darkCharcoal transition-colors duration-300 "
     >
-      히스토리 영역입니다.
+      <div className="">
+        <p className="block text-[2rem] font-extrabold">에프엘이 걸어온 길</p>
+        <div className="mt-4">
+          <Timeline
+            mode="left"
+            items={[
+              {
+                label: "2011-09-01",
+                children: "페스티벌 랜드",
+              },
+              {
+                label: "2012-09-01",
+                children: "무슨 무슨 ",
+              },
+              { label: "2013-09-01", children: "이런 저런" },
+              {
+                label: "2015-09-01",
+                children: "이러쿵 저러쿵",
+              },
+            ]}
+          />
+        </div>
+      </div>
     </section>
   );
 }
