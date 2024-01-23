d38dfd91354259e3a906c20b1302a08c87e002d5
beomsuKim <ta09472@naver.com>
2024-01-23 19:39:00 +0900

    revalidate 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,10 @@ const Contact = dynamic(
 export default function Home() {
   async function getData(): Promise<{ results: Post[] }> {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/notion`);
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/notion`,
+        { next: { revalidate: 600000 } }
+      );
       return await res.json();
     } catch (error) {
       // 이거 에러 핸들링 어케함???
